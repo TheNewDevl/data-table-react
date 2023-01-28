abf55466017f1c036263e63cfcc663943aa93930
carlos <carl.devfs@gmail.com>
2023-01-28 14:43:05 +0100

    update data mocks

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { DataTable } from "./ReactTable";
 import { TableCtxProvider } from "./context/TableContext";
-import { mockEmployees } from "./mocks/mockEmployeesList";
+import { noMissingDataEmployee } from "./mocks/mockEmployeesList";
 import { columns } from "./mocks/columnsMock";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
     <TableCtxProvider>
-      <DataTable data={mockEmployees} columns={columns} />
+      <DataTable data={noMissingDataEmployee} columns={columns} />
     </TableCtxProvider>
   </StrictMode>
 );
--- src/mocks/columnsMock.ts
+++ src/mocks/columnsMock.ts
@@ -3,9 +3,9 @@ import { ColumnType } from "../types";
 export const columns: ColumnType[] = [
   { title: "First Name", data: "firstName" },
   { title: "Last Name", data: "lastName" },
-  { title: "Start Date", data: "startDate" },
+  { title: "Start Date", data: "startDate", type: "date" },
   { title: "Department", data: "department" },
-  { title: "Date of Birth", data: "dateOfBirth" },
+  { title: "Date of Birth", data: "dateOfBirth", type: "date" },
   { title: "Street", data: "street" },
   { title: "City", data: "city" },
   { title: "State", data: "state" },
