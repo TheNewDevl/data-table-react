c0d0cd67867bd4906c675118c0a66158fd5c0008
carlos <carl.devfs@gmail.com>
2023-01-28 16:11:58 +0100

    unit test date fn

---
+++ src/functions/dates/dates.test.ts
@@ -0,0 +1,28 @@
+import { isValidDate } from "./dates";
+import { describe, expect, it } from "vitest";
+
+describe("isValidDate suite test", () => {
+  it("should return true for a valid date in this format MM-DD-YYYY", () => {
+    expect(isValidDate("01-01-2023")).toBe(true);
+  });
+
+  it("should return true for a valid date in this format MM/DD/YYYY", () => {
+    expect(isValidDate("01/01/2023")).toBe(true);
+  });
+
+  it("should return false for an invalid date", () => {
+    expect(isValidDate("01-32-2023")).toBe(false);
+    expect(isValidDate("13-01-2023")).toBe(false);
+    expect(isValidDate("01-01-0000")).toBe(false);
+    expect(isValidDate("invalid date")).toBe(false);
+  });
+
+  it("should return false for a non string", () => {
+    expect(isValidDate(["01-01-20223"])).toBe(false);
+    expect(isValidDate({ date: "01-01-2023" })).toBe(false);
+  });
+  it("should return true for a date instance", () => {
+    const date = new Date();
+    expect(isValidDate(date)).toBe(true);
+  });
+});
--- src/functions/dates/dates.ts
+++ src/functions/dates/dates.ts
@@ -3,5 +3,13 @@
  * @param date - Date to check
  * @returns - True if valid date, false otherwise
  */
-export const isValidDate: Function = (date: any): boolean =>
-  /^\d{2}-|\/\d{2}-|\/\d{4}$/.test(date) && !isNaN(new Date(date).getTime());
+export const isValidDate: (date: any) => boolean = (date) => {
+  if (date instanceof Date) {
+    return true;
+  } else if (typeof date === "string") {
+    return (
+      /^\d{2}-|\/\d{2}-|\/\d{4}$/.test(date) && Number(date.split(/[-/]/)[2]) > 1900 && !isNaN(new Date(date).getTime())
+    );
+  }
+  return false;
+};
