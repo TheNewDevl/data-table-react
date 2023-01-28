ccc55ad4b87330e694e2162caa0be58379f645cc
carlos <carl.devfs@gmail.com>
2023-01-28 15:26:47 +0100

    rename datas

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { DataTable } from "./ReactTable";
 import { TableCtxProvider } from "./context/TableContext";
-import { noMissingDataEmployee } from "./mocks/mockEmployeesList";
+import { noMissingDataEmployees } from "./mocks/mockEmployees";
 import { columns } from "./mocks/columnsMock";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
     <TableCtxProvider>
-      <DataTable data={noMissingDataEmployee} columns={columns} />
+      <DataTable data={noMissingDataEmployees} columns={columns} />
     </TableCtxProvider>
   </StrictMode>
 );
